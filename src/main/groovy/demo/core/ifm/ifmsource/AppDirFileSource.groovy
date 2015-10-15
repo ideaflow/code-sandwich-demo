db643721ba47b449e83a754b18a57aa76f56a8e5
Janelle Klein <janelle@ideaflow.org>
2015-10-15 15:07:31 -0500

    Changed file source to be within branch switch

--- src/main/groovy/demo/core/ifm/ifmsource/AppDirFileSource.groovy
+++ src/main/groovy/demo/core/ifm/ifmsource/AppDirFileSource.groovy
@@ -1,14 +1,16 @@
 package demo.core.ifm.ifmsource
 
 import groovy.io.FileType
+import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Component
 
+@Slf4j
 @Component
-class HomeDirFileSource implements IfmSource {
+class AppDirFileSource implements IfmSource {
 
     private List<IfmTask> ifmTaskList
 
-    HomeDirFileSource() {
+    AppDirFileSource() {
         File baseIdeaFlowDir = getBaseDir()
         baseIdeaFlowDir.mkdirs()
         initIfmTaskList()
@@ -19,17 +21,18 @@ class HomeDirFileSource implements IfmSource {
     }
 
     File getBaseDir() {
-        String userHome = System.getProperty("user.home")
-        new File("$userHome/.ifmfiles")
+        new File("./.ifmfiles")
     }
 
     private void initIfmTaskList() {
         ifmTaskList = []
+        log.info("Base IFM File directory loading from: ${getBaseDir().absolutePath}");
         getBaseDir().eachFileRecurse(FileType.FILES) { file ->
             if(file.name.endsWith('.ifm')) {
                 ifmTaskList.add(new IfmFileTask(baseDir, file))
             }
         }
+        log.info(ifmTaskList.size() + " files loaded.")
     }
 
 }
